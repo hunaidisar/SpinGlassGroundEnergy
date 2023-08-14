c2ff5bf4eeaaeffc9dc34390b78695ef3f9afdd2
hunaidisar <37739827+hunaidisar@users.noreply.github.com>
2023-08-15 01:31:37 +0300

    Add files via upload
    initialized lattice as ising model

--- SpinGlassSettingUptheSystem.py
+++ SpinGlassSettingUptheSystem.py
@@ -2,11 +2,13 @@
 
 
 # env reqs: qiskit nature
-#
+# https://qiskit.org/documentation/stubs/qiskit.algorithms.gradients.FiniteDiffEstimatorGradient.html#qiskit.algorithms.gradients.FiniteDiffEstimatorGradient
+#https://qiskit.org/ecosystem/nature/stubs/qiskit_nature.second_q.hamiltonians.lattices.Lattice.from_adjacency_matrix.html
+#https://qiskit.org/ecosystem/nature/stubs/qiskit_nature.second_q.hamiltonians.lattices.Lattice.to_adjacency_matrix.html
 #-----------------------
 import numpy as np
-from qiskit_nature.second_q.hamiltonians.lattices import Lattice
-from qiskit_nature.second_q.hamiltonians import IsingModel
+from qiskit_nature.second_q.hamiltonians.lattices import Lattice , BoundaryCondition, SquareLattice
+from qiskit_nature.second_q.hamiltonians import IsingModel, LatticeModel
 from qiskit.primitives import Estimator
 from qiskit.algorithms.minimum_eigensolvers import VQE
 from qiskit.algorithms.optimizers import COBYLA, L_BFGS_B, SLSQP
@@ -18,10 +20,33 @@ from qiskit.algorithms.minimum_eigensolvers import NumPyMinimumEigensolver
 
 #%%
 
-interaction = np.array([[5.0, 4.0], [4.0, 5.0]])
+interaction = np.array([[-1, 1.0], [1.0, -1.0]])
 
-lattice = Lattice.from_adjacency_matrix(interaction)
-ising = IsingModel(lattice)
-#print (ising.lattice)
+# lattice = Lattice.from_adjacency_matrix(interaction)
+# ising = IsingModel(lattice)
+# #print (ising.lattice)
 
 estimator = Estimator()
+
+from math import pi
+import numpy as np
+import rustworkx as rx
+from qiskit_nature.second_q.hamiltonians import FermiHubbardModel
+
+#%%
+rows = 5
+cols = 5
+boundary_condition = BoundaryCondition.OPEN
+spin_glass = SquareLattice(rows=rows, cols=cols, boundary_condition=boundary_condition)
+sg_ising = IsingModel(spin_glass)
+print (type (sg_ising))
+#--------
+# Or, initializing an ising model from parameters:
+    # Example
+# interaction = np.array([[4.0, 2.0], [2.0, 4.0]])
+# ising = IsingModel.from_parameters(interaction)
+#-----------
+
+print (spin_glass.draw())
+
+
